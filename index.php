25f0c04591291727355eaf114f1af0242103b165
unknown <itachip38@gmail.com>
2019-01-28 15:53:21 +0900

    create PHP repository

---
+++ index.php
@@ -0,0 +1,15 @@
+<HTML>
+	<HEAD>
+		<TITLE>PHPホーム</TITLE>
+	</HEAD>
+
+	<BODY>
+	<?php
+		$name = 'バープー';
+		echo "<strong>ナマステ、${name}。</strong>";
+	?>
+	<hr>
+	<img src="image/Bengal-Curry.jpg">
+
+	</BODY>
+</HTML>
